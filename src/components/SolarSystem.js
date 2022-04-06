feae063c44102ee1a1465bc79ffa952fc6f2c59c
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-04-06 20:29:52 -0300

    Requisitos 5 e 6 ok

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {
+          planets.map((planeta) => (
+            <PlanetCard
+              key={ planeta.name }
+              planetName={ planeta.name }
+              planetImage={ planeta.image }
+            />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+PlanetCard.propType = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+}.isRequired;
+
+export default SolarSystem;
--- src/components/SolarSystem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class SolarSystem extends React.Component {
-  render() {
-    return <div data-testid="solar-system">a</div>;
-  }
-}
-
-export default SolarSystem;
